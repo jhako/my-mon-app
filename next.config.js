551c19c458f6ab2014ff431903d5751158a9d621
jhako <14149906+jhako@users.noreply.github.com>
2023-06-13 23:23:29 +0900

    ghpages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  output: "export",
+  basePath: "/my-mon-app",
   images: {
     domains: ["images.dog.ceo"],
+    unoptimized: true,
   },
 };
 
